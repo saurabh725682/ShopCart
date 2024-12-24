234ba37241dfc304708ec0c5d61075b39aff0770
Saurabh Maurya <saurabhmaurya725682@gmail.com>
2024-12-24 21:48:23 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 
 # sending emails
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'saurabhmaurya725682@gmail.com'
-EMAIL_HOST_PASSWORD = 'arya70055'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
